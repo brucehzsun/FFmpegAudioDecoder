5fa08f7054fbdf6e00acdb997a9be8676a9391db
hongzhi.sun <hongzhi.sun@rokid.com>
2023-06-01 10:28:12 +0800

    本地联调通过，针对多个包进行容错处理。

--- src/ffmpeg/feed_thread/opus_decoder.cc
+++ src/ffmpeg/feed_thread/opus_decoder.cc
@@ -39,19 +39,19 @@ int RKOpusDecoder::start(format_buffer_write write_buffer, void *opaque_out) {
   opus_decoder_ctl(_opus_decoder, OPUS_SET_BITRATE(bitrate));
 
   int duration = 20;
-  _opu_frame_size = bitrate * duration / 8000;
-  _pcm_frame_size = this->_output_sample_rate * duration / 1000;
-  _pcm_buffer = new uint16_t[_pcm_frame_size];
+//  _opu_frame_size = bitrate * duration / 8000;
+//  _pcm_frame_size = this->_output_sample_rate * duration / 1000;
+  _pcm_buffer = new uint16_t[IO_BUF_SIZE];
 
   return 0;
 }
 
 //const uint16_t *RKOpusDecoder::decode_frame(const void *opu, uint32_t opu_len) {
 int RKOpusDecoder::feed(uint8_t *data, int data_size) const {
-  if (_opus_decoder == NULL) {
+  if (_opus_decoder == nullptr) {
     return -1;
   }
-  if (data == NULL) {
+  if (data == nullptr) {
     return -2;
   }
 
@@ -68,18 +68,15 @@ int RKOpusDecoder::feed(uint8_t *data, int data_size) const {
   std::vector<uint8_t> buffer(num_bytes_to_read);
   std::copy(bytes_to_read, bytes_to_read + num_bytes_to_read, buffer.begin());
 
-  uint16_t *pcm_buffer = new uint16_t[IO_BUF_SIZE];
   int total_samples = 0;
   while (num_bytes_to_read > 0) {
     int num_samples = opus_decode(_opus_decoder, buffer.data(), num_bytes_to_read,
-                                  reinterpret_cast<opus_int16 *>(pcm_buffer), _pcm_frame_size, 0);
+                                  reinterpret_cast<opus_int16 *>(_pcm_buffer), IO_BUF_SIZE, 0);
     if (num_samples < 0) {
-      delete[] pcm_buffer;
       return num_samples;
     }
 
-    (*write_buffer)(opaque_out, reinterpret_cast<uint8_t *>(pcm_buffer), num_samples * sizeof(uint16_t));
-
+    (*write_buffer)(opaque_out, reinterpret_cast<uint8_t *>(_pcm_buffer), num_samples * sizeof(uint16_t));
 
     total_samples += num_samples;
 
@@ -105,7 +102,12 @@ int RKOpusDecoder::feed(uint8_t *data, int data_size) const {
 int RKOpusDecoder::stop() {
   if (_opus_decoder) {
     opus_decoder_destroy(_opus_decoder);
-    _opus_decoder = NULL;
+    _opus_decoder = nullptr;
+  }
+
+  if (_pcm_buffer) {
+    delete[] _pcm_buffer;
+    _pcm_buffer = nullptr;
   }
   return 0;
 }
